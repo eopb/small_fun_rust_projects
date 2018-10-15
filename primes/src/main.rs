87cb977a857bc062aa5f29df47e4d752667ec2d2
Ethan Brierley <ethanboxx@gmail.com>
2018-10-15 14:41:01 +0100

    move primes into function

--- primes/src/main.rs
+++ primes/src/main.rs
@@ -1,13 +1,18 @@
 extern crate rayon;
 
 use rayon::prelude::*;
+use std::ops::RangeFrom;
 
 fn main() {
-    for x in (2..).filter(|x| {
+    for x in primes(2..) {
+        println!("{}", x);
+    }
+}
+
+fn primes(range: RangeFrom<u64>) -> impl Iterator<Item = u64> {
+    range.filter(|x| {
         (2..(*x as f64).sqrt() as u64)
             .into_par_iter()
             .all(|y| x % y != 0)
-    }) {
-        println!("{}", x);
-    }
+    })
 }
