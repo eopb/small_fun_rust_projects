04228cc145e42666c59199306c8d1c9e254a8f1c
Ethan Brierley <ethanboxx@gmail.com>
2018-10-15 18:15:35 +0100

    More functions for making prime numbers

--- primes/src/main.rs
+++ primes/src/main.rs
@@ -1,18 +1,52 @@
 extern crate rayon;
 
 use rayon::prelude::*;
-use std::ops::RangeFrom;
 
 fn main() {
-    for x in primes(2..) {
+    print_prime_range(1000000, 1500000)
+}
+
+fn primes() -> impl Iterator<Item = u64> {
+    (2..).filter(|x| {
+        (2..(((*x as f64).sqrt() as u64) + 1))
+            .into_par_iter()
+            .all(|y| x % y != 0)
+    })
+}
+
+fn prime_range(low: u64, high: u64) -> impl Iterator<Item = u64> {
+    (low..high).filter(|x| {
+        (2..(((*x as f64).sqrt() as u64) + 1))
+            .into_par_iter()
+            .all(|y| x % y != 0)
+    })
+}
+
+fn nth_prime(n: usize) -> u64 {
+    match primes().nth(n) {
+        Some(val) => val,
+        None => panic!("There does not seem to be an {}th prime", n),
+    }
+}
+
+fn print_all_primes() {
+    for x in primes() {
         println!("{}", x);
     }
 }
 
-fn primes(range: RangeFrom<u64>) -> impl Iterator<Item = u64> {
-    range.filter(|x| {
-        (2..(*x as f64).sqrt() as u64)
-            .into_par_iter()
-            .all(|y| x % y != 0)
-    })
+fn print_prime_range(low: u64, high: u64) {
+    for x in prime_range(low, high) {
+        println!("{}", x);
+    }
+}
+
+fn print_x_primes(x: usize) {
+    for x in primes().take(x) {
+        println!("{}", x);
+    }
+}
+
+fn print_nth_prime(n: usize) {
+    println!("{}", nth_prime(n));
 }
