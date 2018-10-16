63dafc3823cf93473dda9d57c55bf84e4664ac3a
Ethan Brierley <ethanboxx@gmail.com>
2018-10-16 12:44:06 +0100

    nvec

---
+++ nvec/src/main.rs
@@ -0,0 +1,13 @@
+#[derive(Clone, Debug)]
+struct Nvec<T>(Vec<NOpt<T>>);
+
+#[derive(Clone, Debug)]
+enum NOpt<T> {
+    Vect(Vec<NOpt<T>>),
+    Value(T),
+}
+
+fn main() {
+    let value = Nvec(vec![NOpt::Vect(vec![NOpt::Value(5); 3]); 3]);
+    println!("{:#?}", value);
+}
