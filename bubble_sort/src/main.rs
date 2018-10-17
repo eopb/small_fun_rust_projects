90daad3a1b33b6260f7c9188b959f88153d496d7
Ethan Brierley <ethanboxx@gmail.com>
2018-10-17 09:49:37 +0100

    remove counting stuff so it is more amazing

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -22,10 +22,7 @@ fn bubble_sort<T, F>(to_sort: &mut [T], f: F) -> &[T]
 where
     F: Fn(&T, &T) -> bool,
 {
-    let mut number_of_reads = 0u128;
-    let mut number_of_swaps = 0u128;
     while let Some(value) = &to_sort.iter().enumerate().find(|(index, value)| {
-        number_of_reads += 1;
         (if *index != &to_sort.len() - 1 {
             (f(value, &to_sort[index + 1]))
         } else {
@@ -33,19 +30,6 @@ where
         })
     }) {
         to_sort.swap(value.0, value.0 + 1);
-        number_of_swaps += 1;
     }
-
-    println!(
-        "number of reads :{}, Number of swaps :{}",
-        number_of_reads, number_of_swaps
-    );
     to_sort
 }
-// if index == sorted.len() - 1 {
-//     continue;
-// } else if f(value, &sorted[index + 1]) {
-//     continue;
-// } else {
-//     sorted.swap(index, index + 1);
-// }
