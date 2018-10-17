54e628f7e243f5e524ed81bdd38634a08ac2a7e0
Ethan Brierley <ethanboxx@gmail.com>
2018-10-17 11:15:26 +0100

    make bubble sort more simple

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -15,13 +15,11 @@ fn bubble_sort<T, F>(to_sort: &mut [T], f: F) -> &[T]
 where
     F: Fn(&T, &T) -> bool,
 {
-    while let Some(value) = &to_sort.iter().enumerate().find(|(index, value)| {
-        if *index != &to_sort.len() - 1 {
-            !f(value, &to_sort[index + 1])
-        } else {
-            false
-        }
-    }) {
+    while let Some(value) = &to_sort
+        .iter()
+        .enumerate()
+        .find(|(index, value)| *index != &to_sort.len() - 1 && !f(value, &to_sort[index + 1]))
+    {
         to_sort.swap(value.0, value.0 + 1);
     }
     to_sort
