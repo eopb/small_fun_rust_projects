4fe8a468e85fec38dd5c882456431d31e43394a7
Ethan Brierley <ethanboxx@gmail.com>
2018-10-18 09:14:34 +0100

    Better match based fizzbuzz

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,16 +1,9 @@
 fn main() {
-    let fizzbuzz = (1..).map(|val| {
-        if val % 3 == 0 {
-            if val % 5 == 0 {
-                "fizzbuzz".to_string()
-            } else {
-                "fizz".to_string()
-            }
-        } else if val % 5 == 0 {
-            "buzz".to_string()
-        } else {
-            val.to_string()
-        }
+    let fizzbuzz = (1..).map(|val| match (val % 3, val % 5) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Fizz".to_string(),
+        (_, 0) => "Buzz".to_string(),
+        (_, _) => val.to_string(),
     });
     for x in fizzbuzz.take(25) {
         println!("{}", x)
