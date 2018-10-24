4ebf35485a9c09edcea6970ecd0a30e6cea2f262
Ethan Brierley <ethanboxx@gmail.com>
2018-10-24 21:17:06 +0100

    use `repeat()` over a for loop

--- staircase/src/main.rs
+++ staircase/src/main.rs
@@ -2,15 +2,11 @@ fn main() {
     println!("{}", staircase(5));
 }
 
-fn staircase(height: u32) -> String {
+fn staircase(height: usize) -> String {
     let mut string = String::new();
     for width in 1..=height {
-        for _ in 1..=height - width {
-            string.push_str(" ")
-        }
-        for _ in 0..width {
-            string.push_str("#")
-        }
+        string.push_str(&" ".repeat(height - width));
+        string.push_str(&"#".repeat(width));
         string.push_str("\n")
     }
     string
