c0b3fb02d0e7b31f8a890dd5d8a569ce87e564e4
Ethan Brierley <ethanboxx@gmail.com>
2018-10-24 18:51:39 +0100

    finish code

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    println!("Hello, world!");
-    let fibonacci = (1..);
-    for x in fibonacci.take(25) {
+    let fibonacci = (0..).map(|n| nthfib(n));
+    for x in fibonacci {
         println!("{}", x)
+    }
+}
+
+fn nthfib(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => nthfib(n - 1) + nthfib(n - 2),
     }
 }
