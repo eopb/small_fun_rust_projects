cbe7ba96ea9aedca174140b85854f529aeecc582
Ethan Brierley <ethanboxx@gmail.com>
2018-10-24 21:06:31 +0100

    use inclusive ranges

--- staircase/src/main.rs
+++ staircase/src/main.rs
@@ -6,10 +6,9 @@ fn main() {
 
 fn staircase(height: u32) -> Vec<String> {
     let mut vec = Vec::new();
-    let height = height + 1;
-    for width in 1..height {
+    for width in 1..=height {
         let mut string = String::new();
-        for _ in 1..height - width {
+        for _ in 1..=height - width {
             string.push_str(" ")
         }
         for _ in 0..width {
