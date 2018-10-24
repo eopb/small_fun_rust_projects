d85a94aec95e96198bd44df2fd7d84d6440dbbef
Ethan Brierley <ethanboxx@gmail.com>
2018-10-24 22:22:55 +0100

    basic working code

--- pythagorean_triplet/src/main.rs
+++ pythagorean_triplet/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let pythagorean_triplets = (1..);
-    for x in pythagorean_triplets.take(25) {
-        println!("{}", x)
+    let mut values_to_test: Vec<[u32; 3]> = Vec::new();
+    let range = 100;
+    for x in 1..=range {
+        for y in 1..=range {
+            for z in 1..=range {
+                if y < x {
+                    values_to_test.push([z, y, x]);
+                }
+            }
+        }
+    }
+    let pythagorean_triplets = values_to_test
+        .iter()
+        .filter(|val| val[0].pow(2) == val[1].pow(2) + val[2].pow(2));
+    for x in pythagorean_triplets {
+        println!("{}^2 = {}^2 + {}^2", x[0], x[1], x[2])
     }
 }
