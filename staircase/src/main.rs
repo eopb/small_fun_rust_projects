03fd227e7fb58980c18b3441882ef9f67adc7f17
Ethan Brierley <ethanboxx@gmail.com>
2018-10-24 20:51:49 +0100

    solve staircase problem

--- staircase/src/main.rs
+++ staircase/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello World")
+    for x in staircase(30) {
+        println!("{}", x);
+    }
+}
+
+fn staircase(height: u32) -> Vec<String> {
+    let mut vec = Vec::new();
+    let height = height + 1;
+    for width in 1..height {
+        let mut string = String::new();
+        for _ in 1..height - width {
+            string.push_str(" ")
+        }
+        for _ in 0..width {
+            string.push_str("#")
+        }
+
+        vec.push(string);
+    }
+    vec
 }
