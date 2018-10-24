ef081a727ebbc2b3149d7986199154a79aec1bba
Ethan Brierley <ethanboxx@gmail.com>
2018-10-24 19:20:29 +0100

    add cache system

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,14 +1,25 @@
+use std::collections::HashMap;
+
 fn main() {
-    let fibonacci = (0..).map(|n| nthfib(n));
-    for x in fibonacci {
+    let mut cache = HashMap::new();
+    let fibonacci = (0..).map(|n| nthfib(n, &mut cache));
+    for x in fibonacci.take(187) {
         println!("{}", x)
     }
 }
 
-fn nthfib(n: u64) -> u64 {
+fn nthfib(n: u128, mut cache: &mut HashMap<u128, u128>) -> u128 {
     match n {
         0 => 0,
         1 => 1,
-        _ => nthfib(n - 1) + nthfib(n - 2),
+        _ => {
+            if let Some(v) = cache.get(&n) {
+                *v
+            } else {
+                let v = nthfib(n - 1, &mut cache) + nthfib(n - 2, &mut cache);
+                cache.insert(n, v);
+                v
+            }
+        }
     }
 }
