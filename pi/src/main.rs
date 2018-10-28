ca96d3986495851f4c96d0091c73ab7bd06fa17e
Ethan Brierley <ethanboxx@gmail.com>
2018-10-28 15:51:48 +0000

    pi needs to mean pi

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    let mut pi = 1.0;
+    let mut pi = 4.0;
     for x in (1..).map(|n| (4.0 * f64::from(n)) - 1.0) {
-        pi -= 1.0 / x;
-        pi += 1.0 / (x + 2.0);
-        println!("{}", pi * 4.0);
+        pi += (1.0 / (x + 2.0) - (1.0 / x)) * 4.0;
+        println!("{}", pi);
     }
 }
