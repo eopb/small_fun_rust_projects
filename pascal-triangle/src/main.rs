bf4cd8e242af3b27f823785cd2be8470e6d0b107
Ethan Brierley <ethanboxx@gmail.com>
2019-12-04 22:14:01 +0000

    work starts

--- pascal-triangle/src/main.rs
+++ pascal-triangle/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let pascal_triangle = (0..).map(|n| {
+        (0..=n).map(move |r| {
+            let f = |x| -> u32 { (1..=x).product() };
+            f(n) / (f(r) * f(n - r))
+        })
+    });
+    let tri_test: Vec<Vec<u32>> = pascal_triangle
+        .take(13)
+        .map(|x| x.collect())
+        .collect::<Vec<_>>();
+    dbg!(tri_test);
 }
